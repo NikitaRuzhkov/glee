99fdc89aa133edc8b0ebafc827718331b73734cf
NikitaRuzhkov <nruzshkov@karazin.ua>
2023-04-23 20:20:29 +0300

    added section products

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,8 @@
 $(function(){
+
+ 
+
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows:false,
@@ -6,4 +10,9 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 2000
   })
+
+  var mixer = mixitup('.products__items');
+ 
+
+
 });
