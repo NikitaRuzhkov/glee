2d3afa32c4552e1ec3c8c4e4595a849a4182b14b
NikitaRuzhkov <nruzshkov@karazin.ua>
2023-04-26 20:07:24 +0300

    added design and video

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,19 @@
-$(function(){
+$(function () {
 
- 
+
 
 
   $('.top-slider__inner').slick({
     dots: true,
-    arrows:false,
+    arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000
   })
 
   var mixer = mixitup('.products__items');
- 
+  var mixer = mixitup('.design__items');
 
+ 
 
 });
