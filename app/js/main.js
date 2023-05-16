c5ad634e64b983084d0348c814c636346db9348d
NikitaRuzhkov <nruzshkov@karazin.ua>
2023-05-16 17:13:00 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,41 @@
 $(function () {
 
 
+  $('.product-tabs__top-item').on('click', function(e){
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+
+  });
+
+
+
+  $('.product-one__input').styler({});
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical:true,
+    draggable: false,
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade:true,
+  });
+
+
+
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
-    onStart:function (data) {
+    onStart: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
@@ -12,7 +43,7 @@ $(function () {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
-    
+
   })
 
   $(".shop-contentlist__star").rateYo({
@@ -21,13 +52,20 @@ $(function () {
     ratedFill: "#ffcc00",
     readOnly: true,
   });
-  
+
   $(".filter-product__star").rateYo({
     starWidth: "11px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     readOnly: true,
   });
+  $(".product-one__star").rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+
 
 
   $('.top-slider__inner').slick({
@@ -41,6 +79,6 @@ $(function () {
   var mixer = mixitup('.products__items');
   var mixer = mixitup('.design__items');
 
- 
+
 
 });
